9521e18b72800686b1747922cd42f8a43a69d889
HaciyevMurad <58561106+HaciyevMurad@users.noreply.github.com>
2020-10-09 18:10:56 +0400

    добаилен модуль tabs.js

---
+++ src/js/tabs.js
@@ -0,0 +1,43 @@
+
+const tabs = (headerSelector, tabSelector, contentSelector, activeClassSelector) =>{
+    const header = document.querySelector(headerSelector),
+          tab = document.querySelectorAll(tabSelector),
+          content = document.querySelectorAll(contentSelector),
+          activeClass = document.querySelector(activeClassSelector);
+
+          function showContent(i=0){
+            content[i].style.display = 'block';
+            tab[i].classList.add(activeClassSelector);
+          }
+
+          function hideContent(){
+              content.forEach(item =>{
+                  item.style.display = 'none';
+              });
+              tab.forEach(item =>{
+                  item.classList.remove(activeClassSelector)
+              });
+          }
+
+          hideContent();
+          showContent()
+
+          header.addEventListener('click', (e)=>{
+            const target = e.target;
+            if(target.classList.contains(tabSelector.replace(/\./, "")) ||
+                target.parentNode.classList.contains(tabSelector.replace(/\./, ''))){
+
+                    tab.forEach((item, i)=>{
+                        if(target == item || target.parentNode == item){
+                            hideContent();
+                            showContent(i)
+                            console.log(target);
+                        }
+                    })
+            }
+          })
+}
+
+
+
+export default tabs
