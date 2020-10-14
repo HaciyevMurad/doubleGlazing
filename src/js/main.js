632938caae3f12adc6550a02de19b2d31e659eb1
HaciyevMurad <58561106+HaciyevMurad@users.noreply.github.com>
2020-10-14 20:23:51 +0400

    as

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,10 @@ import forms from "./forms";
 
 window.addEventListener('DOMContentLoaded' , ()=>{
     modals();
-    tabs('.glazing_slider' , '.glazing_block' ,     '.glazing_content' , 'active')
-    tabs('.decoration_slider' , '.no_click' ,     '.decoration_content > div > div' , 'after_click')
+    tabs('.glazing_slider' , '.glazing_block' , '.glazing_content' , 'active');
+    tabs('.decoration_slider' , '.no_click' , '.decoration_content > div > div' , 'after_click');
+    tabs('.decoration_slider' , '.no_click' , '.decoration_content > div > div' , 'after_click');
+    tabs('.balcon_icons' , '.balcon_icons_img' , '.big_img  > img' , 'do_image_more' , 'inline');
     forms();
    
 })
