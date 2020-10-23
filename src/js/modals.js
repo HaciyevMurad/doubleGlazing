aa1ad0de347b39ba20e3a0775885766d46ad65e3
HaciyevMurad <58561106+HaciyevMurad@users.noreply.github.com>
2020-10-23 16:06:46 +0400

    qqq

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,7 +1,5 @@
 const modals = () => {
-
     function bindModal(triggerSelector, modalSelector, closeSelector , closeOverlayClick = true){
-
             const trigger = document.querySelectorAll(triggerSelector);
             const modal = document.querySelector(modalSelector);
             const close = document.querySelector(closeSelector);
@@ -23,7 +21,6 @@ const modals = () => {
                     });
             });
             
-        
             close.addEventListener('click', (e)=>{
                 modal.style.display = "none";
                 document.body.style.overflow = "";
@@ -33,42 +30,28 @@ const modals = () => {
                     console.log(item)
                 });
         });
-        
             modal.addEventListener("click", (e)=>{
                 if(e.target===modal && closeOverlayClick){
                     windows.forEach(item=>{
                         item.style.display = "none";
                         console.log(item)
                     });
-
                     modal.style.display = "none";
-                    document.body.style.overflow = "";
-
-                    
-                }
-
-                
+                    document.body.style.overflow = "";   
+                }  
             })
-
-
-              
-        
         }
-
         function showModalByTime(selector, time){
             setTimeout(function(){
                 document.querySelector(selector).style.display = "block";
             },60000)
-          
         }
-     
         bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
         bindModal(".phone_link", ".popup", ".popup .popup_close");
         bindModal(".phone_link", ".popup", ".popup .popup_close");
         bindModal(".glazing_price_btn",".popup_calc", ".popup_calc_close" );
         bindModal(".popup_calc_button", ".popup_calc_profile" , ".popup_calc_profile_close", false);
         bindModal(".popup_calc_profile_button" , ".popup_calc_end" , ".popup_calc_end_close" , false)
-
     }
 
 export default modals;
